aa01d7512c7bc346e1e310f48a974d833fd38d1b
Niels Minne <nielsminne@gmail.com>
2024-09-27 16:10:45 +0200

    Les 3

---
+++ week-1/les-3/conditional.js
@@ -0,0 +1,26 @@
+//if - else
+let x = 10;
+
+if(x > 10){
+   console.log("X is groter dan 10");
+}
+if(x < 10){
+    console.log("X is kleiner dan 10")
+}
+if(x === 10){
+    console.log("X is gelijk aan 10 ");
+}
+
+if(x > 10){
+    console.log("X is groter dan 10");
+} else{
+    console.log("X is kleiner of gelijk aan 10");
+}
+
+if(x > 10){
+    console.log("X is groter dan 10");
+} else if(x < 11){
+    console.log("X is kleiner dan 11");
+} else{
+    console.log("X is gelijk aan 10");
+}
---
+++ week-1/les-3/herhaling.js
@@ -0,0 +1,16 @@
+//Data types
+const brand = "BMW"; //String
+const model = "E36 Cabrio"; //String
+const year = 1993; //Number
+const isCar = true; //Boolean
+let x; //UNDEFINED
+
+console.log(model + " is een auto van " + brand);
+console.log("-------------------");
+console.log("deze is gemaakt in het jaar " + year);
+
+console.log(`
+${model} is een auto van ${brand}
+-------------------------------
+deze is gemaakt in het jaar ${year}
+`);
