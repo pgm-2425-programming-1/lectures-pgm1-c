153fe1f4bbfef6384f426e599683a96fc792d351
Niels Minne <nielsminne@gmail.com>
2024-11-05 11:33:52 +0100

    Week 6 - Les 1

---
+++ week-6/les-1/data.js
@@ -0,0 +1,54 @@
+const data = [
+    {
+        id: 1,
+        name: "Voetbal",
+        link: "https://picsum.photos/200?random=1",
+        info: "Klik hier meer info",
+        content: "Voetbal is een teamsport waarbij twee teams van elf spelers ...",
+        timestamp: "2022-01-01"
+    },
+    {
+        id: 2,
+        name: "Basketbal",
+        link: "https://picsum.photos/200?random=2",
+        info: "Klik voor meer info",
+        content: "Basketbal wordt gespeeld door twee teams van vijf spelers op een rechthoekig veld.",
+        timestamp: "2022-01-01",
+    },
+
+    {
+        id: 3,
+        name: "Tennis",
+        link: "https://picsum.photos/200?random=3",
+        info: "Klik voor meer info",
+        content: "Tennis wordt gespeeld door twee of vier spelers met rackets op een veld met een net in het midden.",
+        timestamp: "2022-01-01",
+    },
+
+    {
+        id: 4,
+        name: "Hockey",
+        link: "https://picsum.photos/200?random=4",
+        info: "Klik voor meer info",
+        content: "Hockey is een teamsport waarbij spelers met een stick proberen de bal in het doel te krijgen.",
+        timestamp: "2022-01-01",
+    },
+
+    {
+        id: 5,
+        name: "Zwemmen",
+        link: "https://picsum.photos/200?random=5",
+        info: "Klik voor meer info",
+        content: "Zwemmen is een sport waarbij de atleet verschillende slagen gebruikt om zich door het water voort te bewegen.",
+        timestamp: "2022-01-01",
+    },
+
+    {
+        id: 6,
+        name: "Atletiek",
+        link: "https://picsum.photos/200?random=6",
+        info: "Klik voor meer info",
+        content: "Atletiek omvat verschillende disciplines zoals hardlopen, springen en werpen.",
+        timestamp: "2022-01-01",
+    }
+]
---
+++ week-6/les-1/main.js
@@ -0,0 +1,70 @@
+(() => {
+    const $cardGrid = document.querySelector('#card-grid');
+    const $modal = document.querySelector('#modal');
+
+
+    // function generateHTMLForCard(item) {
+        
+    // }
+    const generateHTMLForCard = (item) => {
+        return `
+            <div class="card" data-id="${item.id}">
+                <img src="${item.link}" alt="${item.name}">
+                <h3>${item.name}</h3>
+                <p>${item.info}</p> 
+            </div>
+        `
+    }
+
+    const generateHTMLForCards = (items) => {
+        let html = "";
+        for (const item of items) {
+            html += generateHTMLForCard(item);
+        }
+
+        return html;
+    }
+
+    const generateModalForCard = (info) => {
+        let $modal = `
+                <div class="modal-content">
+                    <h2 id="modalTitle">${info.name}</h2>
+                    <p id="modalContent">${info.content}</p>
+                    <button id="closeModal">Close</button>
+                </div>
+        
+        `
+        return $modal;
+    }
+    
+    $cardGrid.innerHTML = generateHTMLForCards(data);
+
+    const $cards = document.querySelectorAll('.card');
+    
+    for (const $card of $cards) {
+        $card.addEventListener('click', (event) => {
+            const id = parseInt(event.currentTarget.dataset.id);
+            let clickedCard;
+            for (const item of data) {
+                if (item.id === id)
+                {
+                    clickedCard = item;
+                }
+            }
+            
+            if (clickedCard) {
+                $modal.innerHTML = generateModalForCard(clickedCard);
+                
+                $modal.classList.add('visible');
+                
+            }
+            
+            const $closeModal = document.querySelector('#closeModal');
+            $closeModal.addEventListener('click', () => {
+                $modal.classList.remove('visible');
+            })
+        })
+    }
+
+
+})();
